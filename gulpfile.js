097ffe0cd929ad79301251c34412c4ce0a3f4848
ante <ante@gmail.com>
2017-08-05 17:47:23 +0200

    1.

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require("gulp-rename"),
+    maps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    gzip = require('gulp-gzip'),
+    clean = require('gulp-clean'),
+    spritesmith = require('gulp.spritesmith');
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('img/photos/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('img/sprite'));
+});
+
+gulp.task('concatcss', function () {
+  return gulp.src('css/*.css')
+    .pipe(maps.init())
+    .pipe(concatCss("application.css"))
+    .pipe(maps.write("css"))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('minify-css', ["concatcss"], () => {
+  return gulp.src('css/application.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename("application.min.css"))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('concatScripts', function() {
+  return gulp.src([
+                  'js/jquery.js',
+                  'js/scripts.js',
+                  'js/fastclick.js',
+                  'js/foundation.js',
+                  'js/foundation.equalizer.js',
+                  'js/foundation.reveal.js',
+                  ])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('minify-js',["concatScripts"], function () {
+  return gulp.src('js/app.js')
+  .pipe(uglify())
+  .pipe(rename("app.min.js"))
+  .pipe(gulp.dest('js'))
+});
+
+gulp.task('removeDist', function () {
+    return gulp.src(['dist', "css/application.css", "css/application.min.css", "js/app.js", "js/app.min.js"])
+        .pipe(clean());
+});
+
+gulp.task("build", ["minify-css", "minify-js"], () => {
+  return gulp.src(["css/application.min.css", "js/app.min.js","index.html", "img/**"], {base:"./"})
+            .pipe(gulp.dest("dist"));
+});
+
+gulp.task('default', ["removeDist"], function() {
+  gulp.start("build")
+});
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// 
+// $( ".info1" ).each(function( index, value ) {
+//     index += 1;
+//     $(value).prepend("<img src='img/avatars/avatar" + index + ".jpg' alt='Photo'  class='avatar'>");
+//     // $(value).append("<span class='attribution'>Photo by <b>Werner Bechtelar</b></span>")
+//     // console.log(value)
+//
+//     console.log(value)
+// });
+
+
+
+// <img src="img/avatars/avatar1.jpg" alt="Photo"  class="avatar">
