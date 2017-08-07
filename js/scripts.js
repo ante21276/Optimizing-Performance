000b614d3b7f260dd1cfc980ac8b03b7c96fb479
ante <ante@gmail.com>
2017-08-07 13:45:10 +0200

    5

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
-// 
-// $( ".info1" ).each(function( index, value ) {
-//     index += 1;
-//     $(value).prepend("<img src='img/avatars/avatar" + index + ".jpg' alt='Photo'  class='avatar'>");
-//     // $(value).append("<span class='attribution'>Photo by <b>Werner Bechtelar</b></span>")
-//     // console.log(value)
-//
-//     console.log(value)
-// });
+
+var $link = $(".image");
 
 
+$link.click(function(e) {
 
-// <img src="img/avatars/avatar1.jpg" alt="Photo"  class="avatar">
+  $(".rem").remove();
+  var overlay = "";
+  var $id = "";
+  $id = e.target.parentNode.dataset.revealId;
+  var numero = e.target.getAttribute("data-size")
+  overlay += "<div id='" + $id + "'class='rem reveal-modal' data-reveal aria-hidden='true' role='dialog'>"
+  overlay += "<h2 class='modalTitle'>Photo</h2>"
+  overlay += "<img src='img/photos/photo" + numero + "tiny.jpg' alt='Photo'>"
+  overlay += "<div class='info'>"
+  overlay += "<div class='avatar2 aavatar'></div>"
+  overlay += "<span class='attribution'>Photo by <b>Werner Bechtelar</b></span></div>"
+  overlay += "<a class='close-reveal-modal' aria-label='Close'>&#215;</a></div>"
+
+  $("body").append(overlay);
+});
